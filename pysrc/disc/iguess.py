76294b1a91f190392a28dbc4598c1d26658d5999
Samuel Olivier <smsolivier@gmail.com>
2022-03-17 09:22:39 -0700

    more plots for initial guess

--- pysrc/disc/iguess.py
+++ pysrc/disc/iguess.py
@@ -6,25 +6,27 @@ import matplotlib.pyplot as plt
 from OutputCycler import * 
 oc = OutputCycler()
 
-p = oc.GetOpt(0, 3)
-r = oc.GetOpt(1, 3) 
+r = oc.GetOpt(0, 3) 
+orders = [1,2,3]
 
-base = 'data/disc/iguess/'
-files = ['true', 'false']
-leg = ['Previous', 'Zero']
-outer = []
-tot = []
-for i,f in enumerate(files):
-	df = np.loadtxt(base+f+'_{}_{}.txt'.format(p,r))
-	outer.append(df.shape[0])
-	tot.append(np.sum(df[:,0]))
-	plt.plot(np.arange(1,outer[i]+1), df[:,0], '-o', label=leg[i])
-	plt.xticks(np.arange(1,outer[i]+1, 2))
-assert(len(np.unique(outer))==1)
-plt.legend()
-plt.xlabel('Outer Iteration Number')
-plt.ylabel('Inner Iterations to Convergence')
-if (oc.Good()):
-	plt.savefig(oc.Get())
-else:
+for p in orders:
+	base = 'data/disc/iguess/'
+	files = ['true', 'false']
+	leg = ['Previous', 'Zero']
+	outer = []
+	tot = []
+	plt.figure()
+	for i,f in enumerate(files):
+		df = np.loadtxt(base+f+'_{}_{}.txt'.format(p,r))
+		outer.append(df.shape[0])
+		tot.append(np.sum(df[:,0]))
+		plt.plot(np.arange(1,outer[i]+1), df[:,0], '-o', label=leg[i])
+		plt.xticks(np.arange(1,outer[i]+1, 2))
+	assert(len(np.unique(outer))==1)
+	plt.legend()
+	plt.xlabel('Outer Iteration Number')
+	plt.ylabel('Inner Iterations to Convergence')
+	if (oc.Good()):
+		plt.savefig(oc.Get())
+if not(oc.Good()):
 	plt.show()
