3fe86c64bf4f6a0d79af3701184008cae7d23a8f
Samuel Olivier <smsolivier@gmail.com>
2022-04-17 15:18:54 -0700

    working on discussion

--- pysrc/disc/eps3.py
+++ pysrc/disc/eps3.py
@@ -14,9 +14,10 @@ sweeps = [1,2,3]
 method = 'ip'
 eps = np.arange(1,5)
 
-table = tex.Tabular(r'$\epsilon$', *[str(s) for s in sweeps])
+table = tex.Tabular(r'$\epsilon$', *[str(s) for s in sweeps]*2)
 for e in eps:
 	t = ['$10^{-' + str(e) + '}$']
+	tot = []
 	for s in sweeps:
 		f = open(base+str(s)+'/'+method+str(e)+'.txt', 'r')
 		file = f.read()
@@ -24,14 +25,16 @@ for e in eps:
 		f.close()
 
 		t.append(str(outer))
+		tot.append(str(outer*s))
 
 	# f = open(orthog+method+str(e)+'.txt', 'r')
 	# outer = int(re.findall(r'outer =\s([0-9]*)', f.read())[0])
 	# t.append(str(outer))
 	# f.close()
-
+	t += tot
 	table.AddRow(*t)
-# table.AddColumnGroup('Triple Point', 1, len(sweeps))
+table.AddColumnGroup('Outer Its.', 1, len(sweeps))
+table.AddColumnGroup('Total Sweeps', len(sweeps)+1, len(sweeps))
 # table.AddColumnBreak(0)
 if (oc.Good()):
 	table.Write(oc.Get())
